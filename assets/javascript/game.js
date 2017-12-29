fca2821fc8dafc31dd7fa88650a2099aba4e835a
Mariam Joan <royalton@gmail.com>
2017-12-28 19:26:32 -0800

    updated fight message, characters object literals fixed, healthPoints, attackPower, and counterAttack fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,28 @@
 
-
-var healthPoints, attackPower, counterAttackPower = 0;
-var s1, s2, s3, f1, f2, f3, f4, user, enemy, fighters;
-
-var fight_message = "You attacked " + enemy + " for " + attackPower + " damage, and " + enemy + " attacked you back for " + counterAttackPower + " damage.";
-var won_message = "You have defeated " + enemy + " you can choose to fight another enemy";
-var loss_message = "You have been defeated. Game is over until we battle again.";
-
-var counterAttackPower = Math.floor(Math.random() * 10); // let enemy counterattack === random number
-var attackPower = Math.floor(Math.log(6, 2)); // attack power set to user random base power 6
-var attackFunction, resetGame;
-
 var characters = {
 
-    f1: {
+    fighter_1: {
         name: "Admiral Gial Ackbar",
         attackPower: 90,
         counterAttackPower: 65,
         healthPoints: 150
     },
 
-    f2: {
+    fighter_2: {
         name: "Ona Nobis",
         attackPower: 75,
         counterAttackPower: 65,
         healthPoints: 100 
     },
 
-    f3: {
+    fighter_3: {
         name: "Moff Tiaan Jerjerrod",
         attackPower: 45,
         counterAttackPower: 55,
         healthPoints: 115
     },
 
-    f4: {
+    fighter_4: {
         name: "Poe Dameron",
         attackPower: 65,
         counterAttackPower: 30,
@@ -42,83 +30,120 @@ var characters = {
     }
 };
 
+var copyCharacters = JSON.parse(JSON.stringify(characters))
+var fighter_1, fighter_2, fighter_3, fighter_4;
 
-  var characterData = function() {
+var user, enemy, rest; // divs
 
-    for (var keys in characters) {
-        if (characters.hasOwnProperty(keys)) {
-             keys += characters[keys]
-             return keys;
-        }
-          $("figcapture:id").html(keys);
-    }
+var healthPoints = 0;
+var attackPower;
+var counterAttackPower;
+var defender = "defender";
+var attacker = "attacker";
 
-    characterData();
 
-// Run Javascript when the HTML has finished loading
 $(document).ready(function() {
 
+var characterData = function() {   // here i want to load char data client side 
+    var charArray = Object.keys(characters);
+    for (var i = 0; i < charArray.length; i++) {
+        $("#" + charArray[i] + " > p").html(characters[charArray[i]].healthPoints);
+    }
+};
 
-    var user = $(this);
-    var enemy = $(this);
+characterData();
 
     $("#characters").one("click", "figure", function() {
-        var user = $(this).css("border", "3px solid green")
+        console.log('$(this)', $(this));
+        user = $(this).css("border", "3px solid green")
         $("#your-character").append(user)
-        var rest = $("#characters>div")
+        rest = $("#characters>div")
         $("#enemies-available-to-attack").append(rest).css("border", "3px solid red");
+        attacker = $(this).attr('data-id')
     });
 
-    $("#enemies-available-to-attack").one("click", "figure", function() {
-        var enemy = $(this).css("border", "3px solid black");
-        $("#defender-area").append(enemy)
+    $("#enemies-available-to-attack").on("click", "figure", function() {
+        enemy = $(this).css("border", "3px solid black");
+        $("#defender-area").append(enemy);
+        $("#enemies-available-to-attack").hide();
+        defender = $(this).attr('data-id')
     });
 
-  // ----------------------------------------------------------------
-  // Create functions to enable actions between objects.
-  $("#attack-button").on("click", function() {
+  $("#attack-button").click(function() {
 
-        let user = $("#your-character", "figure"); // let user === figure in yourchar div 
-        let enemy = $("#defender-area", "figure"); // let enemy === figure in defender div  
+    counterAttackPower = Math.floor(Math.random() * 10) + 1; // let enemy counterattack
+    attackPower = Math.floor(Math.random() * 10) + 1; // attackpower set 
 
-        attackPower++; // === user damages enemy
-        $("p:data-id", user).html(user.attackPower); // display innerHTML p:id user attack power
-    
-        counterAttackPower--; // enemy damages user
-        $("p:data-id").html(enemy.counterAttackPower); // display innerHTML p:id enemy attack power
+    console.log("characters[defender]", characters[defender].name)
+    console.log("characters[attacker]", characters[attacker].name)
 
-        healthPoints = user.attackPower - enemy.counterAttackPower; // healthpoints === attack minus counterAttack
-        $("figcapture:id").html(user.healthPoints); // display healthpoints innerHTML figcaption:id user
-        $("figcapture:id").html(enemy.healthPoints); // display healthpoints innerHTML figcaption:id enemy
-        $("#score-status").html(fight_message); // displays damage 
+    console.log("attackPower: ", attackPower)
+    console.log("counterAttackPower: ", counterAttackPower)
+
+    characters[attacker].attackPower = attackPower;
+    characters[defender].counterAttackPower = counterAttackPower;
+
+    console.log("characters[defender].counterAttackPowers ", characters[defender].counterAttackPower)
+    console.log("characters[attacker].attackPower ", characters[attacker].attackPower)
+
+    characters[attacker].healthPoints -= characters[attacker].attackPower * characters[defender].counterAttackPower;
+    characters[defender].healthPoints -= characters[defender].counterAttackPower * characters[attacker].attackPower;
+
+    var fight_message = "You attacked " + characters[defender].name + " for " + characters[attacker].attackPower + " damage, and " + characters[defender].name + " attacked you back for " + characters[defender].counterAttackPower + " damage.";        
+       
+        console.log('attack button clicked')
+
+        user = $("#your-character figure"); // let user === figure in yourchar div 
+        enemy = $("#defender-area figure"); // let enemy === figure in defender div  
+
+        $("#your-character figcaption p").html(characters[attacker].healthPoints); // display healthpoints innerHTML figcaption:id user
+        $("#defender-area figcaption p").html(characters[defender].healthPoints); // display healthpoints innerHTML figcaption:id enemy
+        $("#score-status").text(fight_message); // displays damage 
+});
 
 
-    if (enemy.healthPoints === 0) { // you win if enemy points are zero
-        $("#defender-area", "figure").remove(); // remove img from div
+function winLoss() {
+
+    var won_message = "You have defeated " + characters[defender].attackPower + " you can choose to fight another enemy";
+    var loss_message = "You have been defeated. Game is over until we battle again.";
+
+    if (characters[attacker].healthPoints > 0 && characters[defender].healthPoints <= 0) { // you win if enemy points are zero
+        $("#defender-area figure").remove(); // remove img from div
         $("#score-status").html(won_message); // display won message
+        $("#enemies-available-to-attack").show();
 
         $("#enemies-available-to-attack").on("click", "figure", function() {
-            let enemy = $(this) // move img to new div onclick
-            enemy.append("#defender-area");
+            $("#enemies-available-to-attack").append("#defender-area");
+            defender = $(this).attr('data-id')
+            characters[attacker].healthPoints = copyCharacters[attacker].healthPoints; 
         })
+    }
 
-
-    if (user.healthPoints === 0) { // you lose if your points are zero
-        $("#your-character", "figure").remove(); // remove your img from div
+    else if (characters[defender].healthPoints > 0 && characters[attacker].healthPoints <= 0) { // you lose if your points are zero
+        $("#your-character figure").remove(); // remove your img from div
         $("#score-status").html(loss_message); // display loss message
-        setInterval(resetGame(), 10000); // setInterval to 5 seconds then resetGame function       
-        };
-    };
+    }
+        
+    else if (characters[defender].healthPoints <= 0 && characters[attacker].healthPoints <= 0) {
+        $("#score-status").html("You've both lost. Please use the force and try again!");
+         //setInterval(resetGame(), 10000)
+    } 
+
+    else {
+        $("#score-status").html("Please restart the game!");
+    }
+};
+
+winLoss();
+
+
 });
 
-
-$("#reset-button").on("click", function() {
-// resetGame function onclick innerHTML set attributes to "";
+$("#restart-button").on("click", function() {
     document.querySelector("#score-status", "p:data-id", "figcaption:id").innerHTML = "";
     window.location.reload();
-    });
-
 });
 
-};
+
+
 
